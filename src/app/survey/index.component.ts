ce0713fe4d881fb6da91f2c88a1402bd3fb60cdc
Manav Patel <manav.patel4251@gmail.com>
2021-12-14 16:47:10 -0500

    authentication update

--- src/app/survey/index.component.ts
+++ src/app/survey/index.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/model/auth.service';
+import { Survey } from '../model/survey.model';
+import { SurveyRepository } from "../model/survey.repository";
 
 @Component({
   selector: 'app-index',
@@ -6,5 +10,12 @@ import { Component } from '@angular/core';
   
 })
 export class IndexComponent {
-  constructor(){ }
+  username:string;
+  constructor(public auth: AuthService, private router: Router){
+    this.username=auth.username;
+   }
+
+   surveyList(){
+    this.router.navigateByUrl("survey/list");       
+}
 }
